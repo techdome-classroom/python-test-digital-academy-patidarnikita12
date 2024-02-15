14f9420c57efabdf5d54195ee3c21fa0a19c88a0
Nikita Patidar <patidarnikita36@gmail.com>
2024-02-15 15:36:45 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,10 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        list1 = []
-        myDict ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
-        result1 =0
+        roman_values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+
         for i in range(len(s)):
-            if i<len(s) -1 and myDict[s[i]]<myDict[s[[i+1]]:
-                                            
-        pass
+            # If current value is less than the next value, subtract it
+            if i < len(s) - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+                result -= roman_values[s[i]]
+            else:
+                result += roman_values[s[i]]
+
+        return result
