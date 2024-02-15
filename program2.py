7df6e8f8d6196c076eddb87f4c3470021c08f708
Nikita Patidar <patidarnikita36@gmail.com>
2024-02-15 15:31:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -8,5 +8,5 @@ class Solution(object):
         myDict ={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         result1 =0
         for i in range(len(s)):
-            if i<len(s) -1 and roman_dict[s[i]]<roman_dict
+            if i<len(s) -1 and roman_dict[s[i]]<roman_dict[s[[i+1]]]
         pass
